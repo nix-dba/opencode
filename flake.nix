f330a65b308d33490d7f67f39d6bd5df81301e70
nix <nix@local>
2026-01-26 19:08:03 +0100

    use system config

--- flake.nix
+++ flake.nix
@@ -22,13 +22,18 @@
             "$HOME/.config/containers/policy.json"
         fi
 
-        for entry in ".bun" ".cache" ".config" ".locale"; do
+        for entry in ".bun" ".cache" ".config" ".local"; do
           grep -q "^$entry$" .gitignore 2>/dev/null || echo "$entry" >> .gitignore
         done
 
+        if [ ! -d "$HOME/.config/opencode" ]; then
+          mkdir -p "$HOME/.config/opencode"
+        fi
+
         podman pull ghcr.io/anomalyco/opencode:latest
         exec podman run --userns=keep-id --rm=true -ti --tmpfs /tmp \
-          -v "$PWD:/data" --workdir /data \
+          -v "$PWD:/data" -v "$HOME/.config/opencode:/config" --workdir /data \
+          -e OPENCODE_CONFIG_DIR=/config \
           ghcr.io/anomalyco/opencode:latest "$@"
       '';
     };
