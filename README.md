24edf067db40f2a142915fb9719a9b9f9bcc08ac
nix <nix@local>
2026-01-26 19:00:12 +0100

    init repo

---
+++ README.md
@@ -0,0 +1,11 @@
+# OpenCode
+
+Run opencode in sandboxed container via nix.
+
+## Usage
+
+In your repository root run:
+
+```sh
+nix run github:nix-dba/opencode
+```
---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "OpenCode";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+    
+    opencodeApp = pkgs.writeShellApplication {
+      name = "opencode";
+      runtimeInputs = [ pkgs.podman ];
+      text = ''
+        if ! test -f "$HOME/.config/containers/policy.json"; then
+          mkdir -p "$HOME/.config/containers"
+          install -m 644 ${pkgs.skopeo.src}/default-policy.json \
+            "$HOME/.config/containers/policy.json"
+        fi
+
+        for entry in ".bun" ".cache" ".config" ".locale"; do
+          grep -q "^$entry$" .gitignore 2>/dev/null || echo "$entry" >> .gitignore
+        done
+
+        podman pull ghcr.io/anomalyco/opencode:latest
+        exec podman run --userns=keep-id --rm=true -ti --tmpfs /tmp \
+          -v "$PWD:/data" --workdir /data \
+          ghcr.io/anomalyco/opencode:latest "$@"
+      '';
+    };
+  in {
+    apps.${system}.default = {
+      type = "app";
+      program = "${opencodeApp}/bin/opencode";
+    };
+  };
+}
